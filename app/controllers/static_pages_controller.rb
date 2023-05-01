dd01630a15074b1955c21086f8528a6feaa91370
a2241hm <110039735+a2241hm@users.noreply.github.com>
2023-05-01 01:26:13 +0000

    Finish ch05

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,4 +13,9 @@ class StaticPagesController < ApplicationController
   def about 
     # => /app/views/static_pages/about.html.erb
   end
+
+  def contact 
+    # => /app/views/static_pages/contact.html.erb
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  
   # GET / static_pages#home
   root "static_pages#home"
 
   # URL と アクションが密結合になっている。
   # GET /static_pages => static_pages(controler)#home(action)
   # GET /about => static_pages#about にしていく
-  get  "static_pages/home"
-  get  "static_pages/help"
-  get  "static_pages/about"
+  # get  "static_pages/home"
+  # get  "static_pages/help"
+  # get  "static_pages/about"
+  # get  "static_pages/contact"
+  # get  "users/new"
+
+  get  "/help",    to: "static_pages#help"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
+  get  "/signup",  to: "users#new"
 end
