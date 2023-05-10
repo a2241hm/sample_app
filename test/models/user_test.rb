55f40317cf44f5eee40ea087a43f9b36cf63c37c
a2241hm <110039735+a2241hm@users.noreply.github.com>
2023-05-10 23:56:16 +0000

    Add build script in ch06 for ch07 error

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,7 +20,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "email should be present" do
+  test "email should be unique" do
     @user.email = "     "
     assert_not @user.valid?
   end
@@ -61,6 +61,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email addresses should be saved as lowercase" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+  
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
