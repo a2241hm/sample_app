8b8b604050e226abbafbd784e8c761190cc28827
a2241hm <110039735+a2241hm@users.noreply.github.com>
2023-05-26 11:40:35 +0000

    Finish ch10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   # POST /login
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && 
-      user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
       reset_session
       #params[:session][:remember_me] == '1'  remember(user) : forget(user)
       #三項演算子ではなくこのように書き換えている。
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
         forget(user)
       end      
       log_in user
-      redirect_to user
+      redirect_to forwarding_url || user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
